9545907c81adbe1e6e92b1918123d97864dc8a8b
33themes <33themes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-19 12:30:11 +0000

    Add support for IE11
    git-svn-id: https://plugins.svn.wordpress.org/ttt-devices/trunk@825515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ttt-devices.js
+++ js/ttt-devices.js
@@ -5,19 +5,22 @@ jQuery(document).ready(function($) {
             s = ['ttt-devices'],
             M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*([\d\.]+)/i) || [];
         
-        // if(/trident/i.test(M[1])){
-        //     tem=  /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
-        //     return 'ie '+(tem[1] || '');
-        // }
-        
-        M= M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
-        if((tem= ua.match(/version\/([\d]+)\.*/i))!= null) M[2]= tem[1];
-        if((v = String(M[1]).match(/(\d+)\.*.*/i)) != null) M[1] = v[1];
+        if(/trident/i.test(M[1])){
+            tem=  /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
+            M = ['MSIE', Math.floor(tem[1]) ];
+        }
+        else {
+            M= M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
+            if((tem= ua.match(/version\/(\d+)\.*.*/i))!= null) M[1]= Math.floor(tem[1]);
+            else if((v = String(M[1]).match(/(\d+)\.*.*/i)) != null) M[1] = Math.floor(v[1]);
+        }
+
         if ( /MSIE/.test( M[0] ) ) M[0] = 'ie';
-        if ( /Firefox/.test( M[0] ) || ua.match(/Gecko/i) != null ) M[0] = 'firefox';
+        if ( /Firefox/.test( M[0] ) ) M[0] = 'firefox';
 
         s.push( String(M[0]).toLowerCase() );
         s.push( String(M[0]).toLowerCase()+String(M[1]) );
+        s.push( 'lte-'+String(M[0]).toLowerCase()+String(M[1]+1) );
 
         if (/Linux/.test( navigator.platform )) s.push('linux');
         else if (/Mac/.test( navigator.platform )) s.push('mac');
